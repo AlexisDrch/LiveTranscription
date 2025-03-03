de21aa50a59ca0c02911da16d65954a16528170c
Alexis Durocher <alexis_durocher@apple.com>
2025-03-03 13:39:43 -0800

    Initial Commit

---
+++ LiveTranscription/LiveTranscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTranscriptionApp.swift
+//  LiveTranscription
+//
+//  Created by Alexis Durocher on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveTranscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
